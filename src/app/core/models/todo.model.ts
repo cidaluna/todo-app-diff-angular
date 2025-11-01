4ed69feb90befc6c14d0dc3fe9a9ba4931f99d4f
Cida Luna <cidalunna@hotmail.com>
2025-11-01 12:16:55 -0300

    test todo items

--- src/app/core/models/todo.model.ts
+++ src/app/core/models/todo.model.ts
@@ -1,15 +1,22 @@
 import { Status } from './status.enum';
 
 export interface ITodo {
-  id: number;
+  id: string | number;
   title: string;
   description?: string;
+  channels: string[];
   subtasks: ISubtask[];
   status: Status;
   pendingChange?: Partial<ITodo> | null; // usado na tela diff, é uma propriedade opcional, pode ser undefined e pode conter subconjunto de ITodo
 }
 
 export interface ISubtask {
-  title: string;
+  subtitle: string;
   done: boolean;
+  transaction?: ITransaction[];
+}
+
+export interface ITransaction {
+  label?: string;
+  location?: string[];
 }
